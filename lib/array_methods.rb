bd25ca85fa341f750f0c564bccfaba679aa9c9aa
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-04 17:16:54 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
-  array.length.times do |index|
-    if array[index] == value_to_find
-      return index
+  array.length.times do |count|
+    if array[count] == value_to_find
+      return count
     end
   end
   nil
